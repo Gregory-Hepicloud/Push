106436ad8f872bd8c39ba3773ac6c5caebc337a3
crizip <digibloz@gmail.com>
2014-07-23 23:48:48 +0200

    Bug de text init
    Bug de text init

--- push/gamemode/init.lua
+++ push/gamemode/init.lua
@@ -21,7 +21,7 @@ end
 
 
 
-function GM:PlayerInitialSpawn( ply ) --"When the player first joins the server and spawns" function
+function GM:PlayerInitialSpawn( ply ) --" When the player first joins the server and spawns" function
 
 end
 
@@ -61,13 +61,13 @@ function GM:OnPlayerHitGround( pl )
 end
 
 function KillCounter( victim, killer, weapon )  
-        if killer:GetNWInt("killcounter") == 2 then --nombre de joueur a tuer
-            PrintMessage(HUD_PRINTCENTER, "Le Joueur " .. killer:GetName() .. " Gagne !")  --message de gagne
-            timer.Simple(10, function()   --Sets up a timer for three seconds
-                game.ConsoleCommand("changelevel push_bycrizip \n") --on redemare la map 
+        if killer:GetNWInt("killcounter") == 2 then -- nombre de joueur a tuer
+            PrintMessage(HUD_PRINTCENTER, "Le Joueur " .. killer:GetName() .. " Gagne !")  -- message de gagne
+            timer.Simple(10, function()
+                game.ConsoleCommand("changelevel push_bycrizip \n") -- on redemare la map 
             end)  
         end  
-            killer:SetNWInt("killcounter", killer:GetNWInt("killcounter") + 1)  --ajoute a chaque joueur tuer	
+            killer:SetNWInt("killcounter", killer:GetNWInt("killcounter") + 1)  -- ajoute a chaque joueur tuer	
     end
 	hook.Add("OnNPCKilled","KillCounter", KillCounter)
 	
@@ -94,29 +94,25 @@ function round.Broadcast(Text)
 end
 
 function round.Begin( pl )
-	-- Your code
-	-- (Anything that may need to happen when the round begins)
 	PrintMessage(HUD_PRINTCENTER, "Debut de la partie !")
 	round.Broadcast("Debut de la partie ! Temps restant : " .. round.Time .. " seconds!")
 	timer.Simple(round.Time, round.End)
 end
 
 function round.End()
-	-- Your code
-	-- (Anything that may need to happen when the round ends)
 	PrintMessage(HUD_PRINTCENTER, "FIN DE LA PARTIE !")
 	round.Broadcast("Fin de la partie prochaine partie : " .. round.Break .. " seconds!")
-	timer.Simple(10, function()   --Sets up a timer for three seconds
-                game.ConsoleCommand("changelevel push_bycrizip \n") --on redemare la map 
+	timer.Simple(10, function()   
+                game.ConsoleCommand("changelevel push_bycrizip \n")
             end) 
 	timer.Simple(round.Break, round.Begin)
 end
 
-timer.Simple(10, function(ply)   --Sets up a timer for three seconds
+timer.Simple(10, function(ply)
 round.Begin() 
 end)
 
 
 function GM:PlayerFootstep( ply, pos, foot, sound, volume, rf ) 
-     return true -- Don't allow default footsteps
+     return true
  end
